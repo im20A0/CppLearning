48177b208829a180e3cc83a658ce7d720a3f173a
im20A0 <im20a0@outlook.com>
2022-07-04 20:37:20 +0800

    update

--- removedup.cpp
+++ removedup.cpp
@@ -5,11 +5,11 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         vector<int>::iterator it;
-        int prod=nums[0];
-        for (it=++nums.begin();it!=nums.end();it++) {
-            if (*it==prod) nums.erase(it);
+        int prod = nums[0];
+        for (it=++nums.begin();it!=nums.end();) {
+            if (*it==prod) {nums.erase(it);}
+            else {prod = *it;it++;}
         }
-        
         return nums.size();
     }
 };
@@ -21,7 +21,7 @@ void output(vector<int>& nums) {
     return;
 }
 int main() {
-    vector<int> nums = {1,1,2};
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
     cout<<Solution().removeDuplicates(nums)<<endl;
     output(nums);
     return 0;
