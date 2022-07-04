263edee9d3088ff9861a414a21ae0dd2c89cc37a
im20A0 <im20a0@outlook.com>
2022-07-04 20:23:43 +0800

    update

--- removedup.cpp
+++ removedup.cpp
@@ -6,15 +6,23 @@ public:
     int removeDuplicates(vector<int>& nums) {
         vector<int>::iterator it;
         int prod=nums[0];
-        for (it=nums.begin();it!=nums.end();it++) {
-            cout<<*it<<" ";
+        for (it=++nums.begin();it!=nums.end();it++) {
+            if (*it==prod) nums.erase(it);
         }
         
-        return 0;
+        return nums.size();
     }
 };
+void output(vector<int>& nums) {
+    vector<int>::iterator it;
+    for (it=nums.begin();it!=nums.end();it++) {
+        cout<<*it<<" ";
+    }
+    return;
+}
 int main() {
     vector<int> nums = {1,1,2};
-    Solution().removeDuplicates(nums);
+    cout<<Solution().removeDuplicates(nums)<<endl;
+    output(nums);
     return 0;
 }
